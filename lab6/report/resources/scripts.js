9250d2c500c05fb3e8d2c177d1607390b5334add
Алексей Бабенко <81811659+ShadowChimera@users.noreply.github.com>
2021-04-02 19:42:04 +0300

    Add files via upload

--- lab6/report/resources/scripts.js
+++ lab6/report/resources/scripts.js
@@ -1,46 +1,27 @@
 $(document).ready(function() {
-    $(".changeable-paragraphs > p").click(function(){
-        let mark = $(this).attr("class") ?? "";
-        if(mark.includes("normal-mark") || !mark.length) {
-            $(this).removeClass("normal-mark");
-            $(this).addClass("weight-mark");
-        }
-        else if(mark.includes("weight-mark")) {
-            $(this).removeClass("weight-mark");
-            $(this).addClass("italic-mark");
-        }
-        else if(mark.includes("italic-mark")) {
-            $(this).removeClass("italic-mark");
-            $(this).addClass("underline-mark");
-        }
-        else if(mark.includes("underline-mark")) {
-            $(this).removeClass("underline-mark");
-            $(this).addClass("line-through-mark");
-        }
-        else {
-            $(this).removeClass("line-through-mark");
-            $(this).addClass("normal-mark");
-        }
-    });
+    function animateBlock() {
+        let $block = $(".animated-block");
+        $block.animate({
+            opacity: 0
+        }, 0)
+        .animate({
+            marginLeft: "+=50px",
+            opacity: 1
+        }, 0)
+        .animate({
+            width: `${$block.width() * 2}px`,
+            height: `${$block.height() * 2}px`
+        }, 1000)
+        .animate({
+            marginTop: "+=100px"
+        }, 700)
+        .animate({
+            backgroundColor: "red"
+        }, 1000)
+        .animate({
+            opacity: "0"
+        }, 700);
+    }
 
-    $(".animation > .block").click(function() {
-        $(this).animate({right: "0px"}, 2000)
-                .animate({width: "300px", height: "150px"}, 1000)
-                .animate({left: "0px", top: "50%"}, 2000);
-    });
-
-    $("#order-form > input[type=checkbox]").click(function() {
-        if($(this).is(":checked")) {
-            $radio = $("#order-form > input[type=radio]:first");
-            $radio.attr("disabled", true);
-            $radio.prop("checked", false);
-            $radio =  $("#order-form > input[type=radio]:last");
-            $radio.attr("disabled", true);
-            $radio.prop("checked", false);
-        }
-        else {
-            $("#order-form > input[type=radio]:first").attr("disabled", false);
-            $("#order-form > input[type=radio]:last").attr("disabled", false);
-        }
-    });
+    $("button").on("click", animateBlock);
 });
--- lab6/scripts.js
+++ lab6/scripts.js
@@ -2,25 +2,25 @@ $(document).ready(function() {
     function animateBlock() {
         let $block = $(".animated-block");
         $block.animate({
-            opacity: "0"
+            opacity: 0
         }, 0)
         .animate({
-            "margin-left": `${$block.position().left + 50}px`,
-            opacity: "100%"
+            marginLeft: "+=50px",
+            opacity: 1
         }, 0)
         .animate({
             width: `${$block.width() * 2}px`,
             height: `${$block.height() * 2}px`
         }, 1000)
         .animate({
-            "margin-top": `${$block.position().top + 100}px`
-        }, 500)
+            marginTop: "+=100px"
+        }, 700)
         .animate({
-            backgroundColor: "#112233"
+            backgroundColor: "red"
         }, 1000)
         .animate({
             opacity: "0"
-        }, 500);
+        }, 700);
     }
 
     $("button").on("click", animateBlock);
